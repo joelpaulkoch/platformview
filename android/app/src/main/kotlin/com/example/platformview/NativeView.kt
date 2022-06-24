0548ace574adfb9fcd81e7cdec7bb8eaf3335a1b
Joel Koch <joel.koch@posteo.de>
2022-06-24 10:10:17 +0200

    add flutter project

---
+++ android/app/src/main/kotlin/com/example/platformview/NativeView.kt
@@ -0,0 +1,24 @@
+package com.example.platformview
+
+import android.content.Context
+import android.graphics.Color
+import android.view.View
+import android.widget.TextView
+import io.flutter.plugin.platform.PlatformView
+
+internal class NativeView(context: Context, id: Int, creationParams: Map<String?, Any?>?) : PlatformView {
+    private val textView: TextView
+
+    override fun getView(): View {
+        return textView
+    }
+
+    override fun dispose() {}
+
+    init {
+        textView = TextView(context)
+        textView.textSize = 72f
+        textView.setBackgroundColor(Color.TRANSPARENT)
+        textView.text = "platform view test"
+    }
+}
---
+++ lib/native_view_example.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/foundation.dart';
+import 'package:flutter/gestures.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
+import 'package:flutter/services.dart';
+
+class PlatformWidget extends StatelessWidget {
+  const PlatformWidget({Key? key}) : super(key: key);
+
+  Widget build(BuildContext context) {
+    // This is used in the platform side to register the view.
+    const String viewType = 'platform-view-test';
+    // Pass parameters to the platform side.
+    const Map<String, dynamic> creationParams = <String, dynamic>{};
+
+    return PlatformViewLink(
+      viewType: viewType,
+      surfaceFactory: (context, controller) {
+        return AndroidViewSurface(
+          controller: controller as AndroidViewController,
+          gestureRecognizers: const <Factory<OneSequenceGestureRecognizer>>{},
+          hitTestBehavior: PlatformViewHitTestBehavior.opaque,
+        );
+      },
+      onCreatePlatformView: (params) {
+        return PlatformViewsService.initSurfaceAndroidView(
+          id: params.id,
+          viewType: viewType,
+          layoutDirection: TextDirection.ltr,
+          creationParams: creationParams,
+          creationParamsCodec: const StandardMessageCodec(),
+          onFocus: () {
+            params.onFocusChanged(true);
+          },
+        )
+          ..addOnPlatformViewCreatedListener(params.onPlatformViewCreated)
+          ..create();
+      },
+    );
+  }
+}
